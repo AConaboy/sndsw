73928ef1ba3ef650a452302c593e16ab28545328
Andrew Picot Conaboy <andrew.picot.conaboy@cern.ch>
2022-10-17 16:16:25 +0200

    Commit before first upstream push

--- shipLHC/scripts/SelectionCriteria.py
+++ shipLHC/scripts/SelectionCriteria.py
@@ -28,42 +28,34 @@ class MuonSelectionCriteria(ROOT.FairTask):
         self.runNr = str(options.runNumber).zfill(6)
         self.afswork=options.afswork
         self.afsuser=options.afsuser
+        self.nStations=options.nStations
         self.outpath=self.afswork
         self.EventNumber=-1
 
         self.systemAndPlanes = {1:2,2:5,3:4}
-        #self.systemAndBars={1:7,2:10,3:60}
-        #self.systemAndChannels={1:[8,0],2:[6,2],3:[1,0]}
         self.sdict={0:'Scifi',1:'Veto',2:'US',3:'DS'}
         self.zPos=self.M.zPos
-        #self.cutdists=muAna.GetCutDistributions(self.runNr, 'yresidual')
-        #iterationdict={'zeroth':0, 'ToF':1, 'TW':2}
-        #self.iteration=iterationdict[options.mode]
 
         self.freq=160.316E6
         self.TDC2ns=1E9/self.freq
 
-        #self.largeSiPMmap={0:0 ,1:1 ,3:2 ,4:3 ,6:4 ,7:5}
-        #self.verticalBarDict={0:1, 1:3, 2:5, 3:6}
-        #verticalPlanes=list(self.verticalBarDict.values())
-        #self.xref=42. # To be set as USbarlength/2.
-        #self.correctionfunction=lambda ps, qdc, limit: 1/sum( [ ps[i]*qdc**i for i in range(len(ps)) ] ) 
         self.hists=self.M.h
 
-        self.histtypes=['slopes', 'dy', 'nSiPMs']
+        self.histtypes=['slopes', 'dy', 'nSiPMs','nStations']
+
+        self.hists['nStations'] = ROOT.TH1F('nStations', 'Number of stations used in track fit;Number of stations used;Counts', 10, 0, 10)
+        self.hists['nStations'].Fill(options.nStations)
 
         slopetitle = 'Track slopes;slope x [rad];slope y [rad]'
-        self.hists['slopes'] = ROOT.TH2F('slopes',slopetitle, 300, -1.5, 1.5, 300, -1.5, 1.5)
+        self.hists[f'slopes_{self.nStations}stations'] = ROOT.TH2F(f'slopes_{self.nStations}stations',slopetitle, 300, -1.5, 1.5, 300, -1.5, 1.5)
         for subsystem in (1,2):
             for plane in range(self.systemAndPlanes[subsystem]):
                 
                 yrestitle=f'y-residual_{10*subsystem+plane};track y - bar y-midpoint [cm];track y-residual;Counts / 1 cm'
-                self.hists[f'dy_{10*subsystem+plane}']=ROOT.TH1F(f'dy_{10*subsystem+plane}', yrestitle, 60, -30, 30)
+                self.hists[f'dy_{10*subsystem+plane}_{self.nStations}stations']=ROOT.TH1F(f'dy_{10*subsystem+plane}_{self.nStations}stations', yrestitle, 60, -30, 30)
 
                 nSiPMstitle=f'nSiPMs_{10*subsystem+plane};nSiPMs left;nSiPMs right'
-                self.hists[f'nSiPMs_{10*subsystem+plane}']=ROOT.TH2F(f'nSiPMs_{10*subsystem+plane}', nSiPMstitle,9,0,9,9,0,9)
-
-                # for bar in range(subsystemAndPlanes[subsystem]):        
+                self.hists[f'nSiPMs_{10*subsystem+plane}_{self.nStations}stations']=ROOT.TH2F(f'nSiPMs_{10*subsystem+plane}_{self.nStations}stations', nSiPMstitle,9,0,9,9,0,9)
 
     def GetEntries(self):
         return self.eventTree.GetEntries()
@@ -71,93 +63,80 @@ class MuonSelectionCriteria(ROOT.FairTask):
     def ExecuteEvent(self, event):
         
         hists=self.hists
-        
-        ### 1 fired scintillator per muon subsystem (veto+US for TI18, US for H8)
-        if self.path=='TI18':tmp=(1,2)
-        elif self.path=='H8': tmp=(2,)
-        if not muAna.OneHitPerSystem(event.Digi_MuFilterHits, tmp):return 0
-        DST0cc=muAna.GetDSH_average(event.Digi_MuFilterHits)
-        DST0=self.DST0cut(DST0cc)
-        if DST0==0: return 0
 
-        Reco_MuonTracks=self.M.Reco_MuonTracks
-        theTrack=Reco_MuonTracks[0]
-        fstate=theTrack.getFittedState()
-        pos=fstate.getPos()
-        mom=fstate.getMom()
+        if 'slopes' in self.histtypes:
+            Reco_MuonTracks=self.M.Reco_MuonTracks
+            theTrack=Reco_MuonTracks[0] # For new software update I have to check the unique_ID of the track
+            fstate=theTrack.getFittedState()
+            pos=fstate.getPos()
+            mom=fstate.getMom()
 
-        ### Fill slope hist
-        slopeX, slopeY = mom.x()/mom.z(), mom.y()/mom.z()
-        hists['slopes'].Fill(slopeX, slopeY)
-        
+            ### Fill slope hist
+            slopeX, slopeY = mom.x()/mom.z(), mom.y()/mom.z()
+            hists[f'slopes_{self.nStations}stations'].Fill(slopeX, slopeY)
+
+        # Returns bool for if the track object is in the acceptance for Veto and US respectively.
+        InVeto, InUS=(muAna.InAcceptance(pos, mom, i, self.MuFilter, self.zPos) for i in (1,2))
+        if not InUS: return 0
+
+        # Use the correct subsystems for 1 bar / plane cut
+        if InVeto: tmp=(1,2)
+        else: tmp=(2,)
+        if not muAna.OneHitPerSystem(event.Digi_MuFilterHits, tmp): return 0
+
         for hit in event.Digi_MuFilterHits:
             detID=hit.GetDetectorID()
             s,p,b=muAna.parseDetID(detID)
             if s==3: continue
-            # if not self.yresidual(detID,pos,mom): continue
-            
-            zEx=self.zPos['MuFilter'][s*10+p]
-            lam=(zEx-pos.z())/mom.z()
-            Ex=ROOT.TVector3(pos.x()+lam*mom.x(), pos.y()+lam*mom.y(), pos.z()+lam*mom.z())
 
-            xpred=self.xpred(v1,Ex)
-            xpred_R=-xpred
+            if 'slopes' in self.histtypes:
+                zEx=self.zPos['MuFilter'][s*10+p]
+                lam=(zEx-pos.z())/mom.z()
+                Ex=ROOT.TVector3(pos.x()+lam*mom.x(), pos.y()+lam*mom.y(), pos.z()+lam*mom.z())
 
-            ### Fill y-residual histogram
-            self.Fillyresidual(detID,pos,mom,Ex)
+                xpred=self.xpred(v1,Ex)
+                xpred_R=-xpred
+
+                ### Fill y-residual histogram
+                self.Fillyresidual(detID,pos,mom,Ex)
 
             ### Fill nSiPMs histogram
             self.FillnSiPMs(detID, hit)
 
     def Fillyresidual(self,detID,pos,mom,Ex):
+
+        self.MuFilter.GetPosition(detID,v1,v2)
+        dy=Ex.y()-v1.y() # Needs to be checked
         s,p,b=muAna.parseDetID(detID)
         key=10*s+p
         z=self.zPos['MuFilter'][key]
         lam=(z-pos.z())/mom.z()
+        pq = v1-pos
+        uCrossv= (v2-v1).Cross(mom)
+        doca = pq.Dot(uCrossv)/uCrossv.Mag()
 
-        self.MuFilter.GetPosition(detID,v1,v2)
-        dy=Ex.y()-v1.y() # Needs to be checked
-
-        self.hists[f'dy_{key}'].Fill(dy)
+        self.hists[f'dy_{key}_{self.nStations}stations'].Fill(doca)
 
     def FillnSiPMs(self, detID, hit):
         nLeft, nRight=muAna.GetnFiredSiPMs(hit)
         s,p,b=muAna.parseDetID(detID)
         key=10*s+p
-        self.hists[f'nSiPMs_{key}'].Fill(nLeft, nRight)
+        self.hists[f'nSiPMs_{key}_{self.nStations}stations'].Fill(nLeft, nRight)
 
     def WriteOutHistograms(self):
         
         for h in self.M.h:
 
-                hist=self.M.h[h]
-                outpath=f'{self.afswork}/splitfiles/run{self.runNr}/SelectionCriteria/'
-                path_obj=Path(outpath)
-                path_obj.mkdir(parents=True, exist_ok=True)
-                
-                outfile=f'SelectionCriteria_{self.options.nStart}.root'
-                f=ROOT.TFile.Open(outpath+outfile, 'update')
-                f.WriteObject(hist, hist.GetName(), 'kOverwrite')
-                f.Close()
+            hist=self.M.h[h]
+            outpath=f'{self.afswork}/splitfiles/run{self.runNr}/SelectionCriteria/'
+            path_obj=Path(outpath)
+            path_obj.mkdir(parents=True, exist_ok=True)
+            
+            outfile=f'SelectionCriteria_{self.options.nStart}.root'
+            f=ROOT.TFile.Open(outpath+outfile, 'update')
+            f.WriteObject(hist, hist.GetName(), 'kOverwrite')
+            f.Close()
         print(f'{len(self.M.h)} histograms saved to {outpath}{outfile}')
-
-    @staticmethod
-    def dycut(hist, nsig=1):    
-        dymin=hist.GetMean()-nsig*hist.GetStdDev()
-        dymax=hist.GetMean()+nsig*hist.GetStdDev()
-        return dymin, dymax
-
-    @staticmethod
-    def slopecut(mom, slopecut=0.1):
-        slopeX, slopeY=mom.x()/mom.z(), mom.y()/mom.z()
-        if abs(slopeX)>slopecut or abs(slopeY)>slopecut: return 0
-        else: return 1
-
-    def DST0cut(self,DST0):
-        if DST0==-999. or DST0==-998. or DST0==-6237.5: return 0
-        DST0ns=DST0*6.25
-        if DST0ns<13.75 or DST0ns>15.45: return 0
-        return DST0ns 
     
     def xpred(self,v1,Ex):
         return ROOT.TMath.Sqrt((v1.x()-Ex.x())**2+(v1.y()-Ex.y())**2+(v1.z()-Ex.z())**2)
--- shipLHC/scripts/TimeWalk.py
+++ shipLHC/scripts/TimeWalk.py
@@ -32,12 +32,12 @@ class TimeWalk(ROOT.FairTask):
         self.outpath=self.afswork
         self.EventNumber=-1
 
-        self.systemAndPlanes = {1:2,2:5,3:4}
+        self.systemAndPlanes = {1:2,2:5,3:7}
         self.systemAndBars={1:7,2:10,3:60}
         self.systemAndChannels={1:[8,0],2:[6,2],3:[1,0]}
         self.sdict={0:'Scifi',1:'Veto',2:'US',3:'DS'}
         self.zPos=self.M.zPos
-        self.cutdists=muAna.GetCutDistributions(self.runNr, 'yresidual')
+        self.cutdists=muAna.GetCutDistributions(self.runNr, 'dy', options.nStations)
         iterationdict={'zeroth':0, 'ToF':1, 'TW':2}
         self.iteration=iterationdict[options.mode]
 
@@ -50,6 +50,9 @@ class TimeWalk(ROOT.FairTask):
         self.xref=42. # To be set as USbarlength/2.
         self.correctionfunction=lambda ps, qdc, limit: 1/sum( [ ps[i]*qdc**i for i in range(len(ps)) ] ) 
         self.hists=self.M.h
+
+        self.hists['nStations'] = ROOT.TH1F('nStations', 'Number of stations used in track fit;Number of stations used;Counts', 10, 0, 10)
+        self.hists['nStations'].Fill(options.nStations)        
         
         if options.mode=='TW':
             if not options.CorrectionType: self.CorrectionType=4
@@ -67,30 +70,39 @@ class TimeWalk(ROOT.FairTask):
 
         hists=self.hists
         
-        ### 1 fired scintillator per muon subsystem (veto+US for TI18, US for H8)
-        if self.path=='TI18':tmp=(1,2)
-        elif self.path=='H8': tmp=(2,)
-        if not muAna.OneHitPerSystem(event.Digi_MuFilterHits, tmp):return 0
-        DST0cc=muAna.GetDSH_average(event.Digi_MuFilterHits)
-        DST0=self.DST0cut(DST0cc)
-        if DST0==0: return 0
-
         Reco_MuonTracks=self.M.Reco_MuonTracks
         theTrack=Reco_MuonTracks[0]
         fstate=theTrack.getFittedState()
         pos=fstate.getPos()
         mom=fstate.getMom()
 
+        # Returns bool for if the track object is in the acceptance for Veto and US respectively.
+        InVeto, InUS=(muAna.InAcceptance(pos, mom, i, self.MuFilter, self.zPos) for i in (1,2))
+        if not InUS: return 0
+
+        # Use the correct subsystems for 1 bar / plane cut
+        if InVeto: tmp=(1,2)
+        else: tmp=(2,)
+        if not muAna.OneHitPerSystem(event.Digi_MuFilterHits, tmp): return 0
+
+        # Get DS event t0
+        DST0cc=muAna.GetDSH_average(event.Digi_MuFilterHits)
+        if not 'DST0' in self.hists:
+           self.hists['DST0']=ROOT.TH1F('DST0','DSH average', 100, 0, 25)
+        self.hists['DST0'].Fill(DST0cc*6.25)
+        DST0=self.DST0cut(DST0cc)
+        if DST0==0: return 0
+
         ### Slope cut
         slopeX, slopeY = mom.x()/mom.z(), mom.y()/mom.z()
         if not TimeWalk.slopecut(mom): return 0
-        
+
         for hit in event.Digi_MuFilterHits:
             detID=hit.GetDetectorID()
             s,p,b=muAna.parseDetID(detID)
             if s==3: continue
             if not self.yresidual(detID,pos,mom): continue
-            
+
             zEx=self.zPos['MuFilter'][s*10+p]
             lam=(zEx-pos.z())/mom.z()
             Ex=ROOT.TVector3(pos.x()+lam*mom.x(), pos.y()+lam*mom.y(), pos.z()+lam*mom.z())
@@ -112,33 +124,32 @@ class TimeWalk(ROOT.FairTask):
                 elif self.options.mode=='TW': self.TW(fixed_ch, xpred, clock, qdc, DST0)
 
     def zeroth(self,fixed_ch,xpred,clock,qdc,DST0):
-        hists=self.M.h
+        hists=self.hists
         
         detID, SiPM=( int(fixed_ch.split('_')[i]) for i in range(2) ) 
 
         correctedtime=clock*6.25
         t_rel=DST0-correctedtime
-        name=f'dtvxpred_{fixed_ch}_iteration{self.iteration}'
-        if not name in hists:
+        dtvxpred=f'dtvxpred_{fixed_ch}_iteration{self.iteration}'
+        if not dtvxpred in hists:
            title='Uncorrected T_{0}^{DS}-t_{'+str(SiPM)+'} v x-position;x_{predicted} [cm];T_{0}^{DS}-t_{'+str(SiPM)+'} [ns]'
-           hists[name]=ROOT.TH2F(name,title,110,-10,100, 160, -20, 20.)
-        if not 'DST0' in self.hists:
-           hists['DST0']=ROOT.TH1F('DST0','DSH average', 100, 0, 25)
-        name2=f'tSiPM_{fixed_ch}_iteration{self.iteration}'
-        if not name2 in hists:
-           hists[name2]=ROOT.TH1F(name2,f'Measured SiPM time {fixed_ch};SiPM time [ns];Counts', 100, 0, 25)
-        name3=f'attlen_{fixed_ch}_iteration{self.iteration}'
-        if not name3 in hists:
+           hists[dtvxpred]=ROOT.TH2F(dtvxpred,title,110,-10,100, 160, -20, 20.)
+
+        SiPMtime=f'tSiPM_{fixed_ch}_iteration{self.iteration}'
+        if not SiPMtime in hists:
+           hists[SiPMtime]=ROOT.TH1F(SiPMtime,f'Measured SiPM time {fixed_ch};SiPM time [ns];Counts', 100, 0, 25)
+        attlen=f'attlen_{fixed_ch}_iteration{self.iteration}'
+        if not attlen in hists:
             title='Predicted position against QDC_{'+str(SiPM)+'} '+fixed_ch+';x_{predicted} [cm];QDC_{'+str(SiPM)+'} [a.u]'
-            hists[name3]=ROOT.TH2F(name3,title, 110, -10, 110, 200, 0., 200)
+            hists[attlen]=ROOT.TH2F(attlen,title, 110, -10, 110, 200, 0., 200)
+        # trackposveto0=f'DS muon track position at veto 0;Track x [cm];Track y [cm],'    
         
-        self.hists[name].Fill(xpred,t_rel)
-        self.hists[name2].Fill(correctedtime)
-        self.hists['DST0'].Fill(DST0)
-        self.hists[name3].Fill(xpred, qdc)
+        self.hists[dtvxpred].Fill(xpred,t_rel)
+        self.hists[SiPMtime].Fill(correctedtime)
+        self.hists[attlen].Fill(xpred, qdc)
 
     def ToF(self, fixed_ch, xpred, clock, qdc, DST0):
-        hists=self.M.h
+        hists=self.hists
 
         cdata=muAna.Getcscint(self.runNr, fixed_ch, self.iteration-1)
         if cdata==-999.: return 0
@@ -152,7 +163,7 @@ class TimeWalk(ROOT.FairTask):
         self.hists[name].Fill(qdc,t_rel)
 
     def TW(self, fixed_ch, xpred, clock, qdc, DST0):
-        hists=self.M.h
+        hists=self.hists
         
         SiPM=int(fixed_ch.split('_')[-1])
         time=clock*6.25
@@ -202,6 +213,10 @@ class TimeWalk(ROOT.FairTask):
         hists[name].Fill(xpred,t_rel)
         hists[name2].Fill(qdc, t2_rel)
 
+    def FillDST0(self):
+        pass 
+
+
     def WriteOutHistograms(self):
         
         for h in self.M.h:
@@ -221,18 +236,18 @@ class TimeWalk(ROOT.FairTask):
                 f.Close()
         print(f'{len(self.M.h)} histograms saved to {self.afswork}/run{self.runNr}')
 
-    def yresidual(self,detID,pos,mom):
-        s,p,b=muAna.parseDetID(detID)
-        key=10*s+p
-        z=self.zPos['MuFilter'][key]
-        lam=(z-pos.z())/mom.z()
-        Ex=ROOT.TVector3(pos.x()+lam*mom.x(), pos.y()+lam*mom.y(), pos.z()+lam*mom.z())
-        self.MuFilter.GetPosition(detID,v1,v2)
-        dy=Ex.y()-v1.y() # Needs to be checked
-        dy_min, dy_max = TimeWalk.dycut(self.cutdists[f'{key}_yresidual'])
-        #if dymin or dy
-        if dy>dy_max or dy<dy_min: return 0
-        else: return 1
+    def yresidual(self,detID,pos,mom): 
+        s,p,b=muAna.parseDetID(detID) 
+        key=10*s+p 
+        z=self.zPos['MuFilter'][key] 
+        lam=(z-pos.z())/mom.z() 
+        Ex=ROOT.TVector3(pos.x()+lam*mom.x(), pos.y()+lam*mom.y(), pos.z()+lam*mom.z()) 
+        self.MuFilter.GetPosition(detID,v1,v2) 
+        dy=Ex.y()-v1.y() # Needs to be checked 
+        dy_min, dy_max = TimeWalk.dycut(self.cutdists[f'dy_{key}_{self.options.nStations}stations']) 
+        #if dymin or dy 
+        if dy>dy_max or dy<dy_min: return 0 
+        else: return 1 
     
     @staticmethod
     def dycut(hist, nsig=1):    
@@ -241,7 +256,7 @@ class TimeWalk(ROOT.FairTask):
         return dymin, dymax
 
     @staticmethod
-    def slopecut(mom, slopecut=0.1):
+    def slopecut(mom, slopecut=0.4):
         slopeX, slopeY=mom.x()/mom.z(), mom.y()/mom.z()
         if abs(slopeX)>slopecut or abs(slopeY)>slopecut: return 0
         else: return 1
